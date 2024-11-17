ca3a597e2b11b4b7e2bc72c4652997062d05efec
tasnimalam1 <rafsantasnimrafsan@gmail.com>
2024-11-17 23:23:53 +0600

    vocabulary learning application

---
+++ src/routes/router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1>hello from router</h1>
+    }
+]);
+
+export default router;
